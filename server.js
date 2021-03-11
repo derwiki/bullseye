0dee40e82b7b18dd9e4ba6941c181203e0c8ac48
Adam Derewecki <aderewecki@lyft.com>
2021-03-11 10:14:21 -0800

    changes

--- server.js
+++ server.js
@@ -12,9 +12,36 @@ const server = express()
 
 const io = socketIO(server);
 
+class Game {
+  constructor(seed) {
+    // generate map??? i dunno
+  }
+
+  assignPlayer(ab, socket) {
+    // sets who is each player
+  }
+
+  updatePlayer(ab, data) {
+    // just assign the velocity/tilt
+  }
+
+  tick() {
+    // broadscast calc game state
+  }
+}
+
+const games = {
+  roomId: Game.new()
+}
+
 io.on('connection', (socket) => {
   console.log('Client connected');
+
+  socket.on('playerLogIn', (response) => {
+    console.log('server player log in', response);
+  })
   socket.on('disconnect', () => console.log('Client disconnected'));
 });
+
 
 setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
