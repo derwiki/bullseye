d40b5128ae68a0480439c77b5383690a14b4ce5c
Adam Derewecki <aderewecki@lyft.com>
2021-03-11 10:36:07 -0800

    more js

--- server.js
+++ server.js
@@ -11,10 +11,17 @@ const server = express()
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 const io = socketIO(server);
-
 class Game {
-  constructor(seed) {
+  constructor(io) {
+    this.io = io;
+    setInterval(() => {
+      this.tick();
+    }, 250);
     // generate map??? i dunno
+  }
+
+  setSocket(socket) {
+    this.socket = this.socket || socket;
   }
 
   assignPlayer(ab, socket) {
@@ -26,16 +33,29 @@ class Game {
   }
 
   tick() {
+    this.io.emit('gameState',  {
+      io: 2
+    });
+    if (this.socket) {
+      this.socket.emit('gameState', {
+        foo: 42
+      });
+    } else {
+      console.log('no this.socket')
+    }
+    
     // broadscast calc game state
   }
 }
 
 const games = {
-  //
+  foo: new Game(io)
 };
 
 io.on('connection', (socket) => {
   console.log('Client connected');
+
+  games.foo.setSocket(socket);
 
   socket.on('playerLogIn', (response) => {
     console.log('server player log in', response);
